e3778fbf6bca72947b9ffeede4b1f7eff3667651
Rahul Patle <141508051+mentei@users.noreply.github.com>
2025-06-21 00:00:06 +0530

    rust pre-requisists added

---
+++ rust_airdrop/target/debug/deps/rust_airdrop.d
@@ -0,0 +1,5 @@
+D:\turbine3\rust_airdrop\target\debug\deps\rust_airdrop.exe: src\main.rs
+
+D:\turbine3\rust_airdrop\target\debug\deps\rust_airdrop.d: src\main.rs
+
+src\main.rs:
---
+++ rust_airdrop/target/debug/rust_airdrop.d
@@ -0,0 +1,1 @@
+D:\turbine3\rust_airdrop\target\debug\rust_airdrop.exe: D:\turbine3\rust_airdrop\src\main.rs
